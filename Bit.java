0b15e8a5c832fddfab6d7a257bca7535d76fa14e
SEO2317 <107653158+SEO2317@users.noreply.github.com>
2022-10-28 20:54:52 +0900

    Create Bit.java

---
+++ Bit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution{
+    public String solution(int n, int t, int m, int p){
+        StringBuilder convert = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i=0;convert.length()<=t*m; i++){     //t= 숫자 m=인원 
+            convert.append(Integer.toString(i,n));
+        }
+                           
+        for( int i = p - 1; answer.length()<t;i+=m){
+             answer.append(convert.charAt(i));
+        }
+        return answer.toString().toUpperCase();
+    }
+}
